e31d3bf7743bfc620a3962501a0373190c77ac1a
Claudio Herrera <herrerac11@gmail.com>
2018-04-12 21:45:33 -0400

    Improve skeleton for build script

--- build.sh
+++ build.sh
@@ -1,13 +1,40 @@
-# build.sh
+# build.sh - Claudio Herrera
+echo "Initializing new project..."
+sleep 1s
 npm init
+
+echo "Bootstrapping create-react-app..."
+sleep 1s
 npx create-react-app client
-echo "Appending proxy to create-react-app for local development servers..."
-cd client
-# add proxy to package.json
+
 echo "Adding server dependencies..."
-# npm install --save...
+sleep 1s
+npm install --save body-parser cookie-session express mongoose path-parser
+echo "Adding server dev dependencies..."
+sleep 1s
+npm install --save-dev concurrently nodemon
+echo "Adding script helpers to server..."
+sleep 1s
+# Add scripts to server
+# "start": "node index.js",
+# "server": "nodemon index.js",
+# "client": "npm run start --prefix client",
+# "dev": "concurrently \"npm run server\" \"npm run client\" \"npm run webhook\""
 echo "Creating directories for server..."
+sleep 1s
 mkdir config middleware models routes services
+
 echo "Adding client dependencies..."
+sleep 1s
 cd client
-# npm install --save...
+npm install --save axios react-redux react-router-dom redux
+echo "Appending proxy to create-react-app for local development servers..."
+sleep 1s
+# "proxy": {
+#    "/api/*": {
+#      "target": "http://localhost:5000"
+#    }
+# }
+echo "Populating files for first development run..."
+sleep 1s
+# Populate server/client files
